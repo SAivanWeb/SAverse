952e7eba1cd30211ed2354563da4afd6f645edcc
Иван <istepanchikov@scid.ru>
2025-10-21 23:08:35 +0300

    initial

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // PUBLIC ROUTES
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/public/HomeView.vue'),
+      meta: { requiresGuest: true }
+    },
+
+    // PROTECTED ROUTES
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/DashboardView.vue'),
+      meta: { requiresAuth: true }
+    },
+    // FALLBACK
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
+    }
+  ]
+})
+
+export default router
